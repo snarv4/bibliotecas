1b60bc6979d11fb7fde77d1f694c54f0ea181cb4
snarv4 <snarvaez@gbmpaulovi.edu.ec>
2025-10-29 16:55:16 -0500

    Segunda parte
    Inicializamos los promedios de los alumnos

---
+++ notas.py
@@ -0,0 +1,24 @@
+alumnos = [
+    {
+        "nombre": "Ana",
+        "notas": [8.0,7.5,8.5]
+    },
+    {
+        "nombre": "Damian",
+        "notas": [9.0,6.5,9.5]
+    },
+    {
+        "nombre": "Benito",
+        "notas": [10.0,8.5,6.5]
+    }
+]
+
+suma_promedios_individuales = 0
+contador_alumnos = 0
+
+for alumno in alumnos:
+    # Capturando los nombres y las notas de mis bibliotecas
+    nombre = alumno["nombre"]
+    notas = alumno["notas"]
+    promedio_individual = sum(notas) / len(notas)
+    print(promedio_individual)
